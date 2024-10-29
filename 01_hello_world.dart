9492550a6f157b86efe98d17587ce3c5439d6a41
luis <agencia.b2b.ve@gmail.com>
2024-10-29 18:31:18 -0400

    first commit

---
+++ 01_hello_world.dart
@@ -0,0 +1,12 @@
+void main() {
+  //var myName = 'Luis';
+  //String myName = 'Luis';
+  //late final myName;
+  const myName = 'Luis';
+  
+  //myName = 'Luis';
+
+  print('Hola $myName');
+  print('Hola ${myName.toUpperCase()}');
+  print('Hola ${ 1 + 1 }');
+}
---
+++ 05_functions.dart
@@ -0,0 +1,15 @@
+void main(){
+  
+  print( greetEveryone() );
+  print('suma: ${addTwoNumbers(10, 20)} ');
+}
+
+String greetEveryone() => 'Hello everyone!';
+
+int addTwoNumbers( int a, int b ) => a + b;
+
+int addTwoNumbersOptional( int a, [ int b = 0 ] ){
+  
+  //b ??= 0;
+  return a + b;
+}
