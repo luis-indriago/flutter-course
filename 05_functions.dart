2d6f4182beff3928cd9c301f8b9b7f1330a061d4
luis <agencia.b2b.ve@gmail.com>
2024-11-15 17:19:58 -0400

    second commit

--- 05_functions.dart
+++ 05_functions.dart
@@ -1,7 +1,10 @@
 void main(){
   
   print( greetEveryone() );
-  print('suma: ${addTwoNumbers(10, 20)} ');
+  
+  print('Suma: ${addTwoNumbers(10, 20)} ');
+  
+  print( greetPerson( name: 'Luis', message: 'Hi' ) );
 }
 
 String greetEveryone() => 'Hello everyone!';
@@ -12,4 +15,10 @@ int addTwoNumbersOptional( int a, [ int b = 0 ] ){
   
   //b ??= 0;
   return a + b;
+}
+
+
+String greetPerson({ required String name, String message = 'Hola,' }){
+  
+  return '$message $name';
 }
